0a476fe414bceee4f6fb1b560087dfd5b88ac133
Kavi1808 <37444046+Kavi1808@users.noreply.github.com>
2018-05-24 10:03:13 +0530

    Create repeated letter.py

---
+++ repeated letter.py
@@ -0,0 +1,10 @@
+import collections
+str1 = 'kavitha'
+d = collections.defaultdict(int)
+for c in str1:
+    d[c] += 1
+
+for c in sorted(d, key=d.get, reverse=True):
+  if d[c] > 1:
+      print( (c, d[c]))
+  
